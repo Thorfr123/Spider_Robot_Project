b4d38d4d1db1a1a4c669512ddcce35fb2701c73a
Jorge Ribeiro <109107004+Thorfr123@users.noreply.github.com>
2023-02-03 15:42:42 +0000

    Update servos.h

--- include/servos.h
+++ include/servos.h
@@ -44,7 +44,7 @@ float calc_time(int servoID, int angle);
 // Update servo angle
 void update_angle(int servoID, int angle, int servoPIN);
 
-// Reset servo angle (NEED TO BE CHANGED LATER)
+// Reset servo angle
 void reset_angle(int servoID, int servoPIN);
 
 // Checks whether the servo has reached the intended angle
